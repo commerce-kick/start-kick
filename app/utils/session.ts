c9c66f1ac208152ccbad28b981971eca346649da
fermin villanueva <fervillanuev4s@gmail.com>
2025-05-29 14:09:55 -0700

    fix: correct indentation in useAppSession function for better readability

--- app/utils/session.ts
+++ app/utils/session.ts
@@ -3,6 +3,9 @@ import { useSession } from "@tanstack/react-start/server";
 
 export function useAppSession() {
   return useSession<SalesforceSessionData>({
-    password: process.env.SESSION_SECRET || "your-session-secret-here-asd-a-sd-asd-as-d-as-da-sd-as",
+    name: "SLAS",
+    password:
+      process.env.SESSION_SECRET ||
+      "your-session-secret-here-asd-a-sd-asd-as-d-as-da-sd-as",
   });
 }
