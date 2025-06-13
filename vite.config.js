67c77bdd951a1b24aacc42be03031bfdb1a8a965
Fermin Villanueva Salas <FerVillanuev4s@gmail.com>
2025-06-12 17:36:35 -0700

    refactor: streamline tanstackStart configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
-
 
 export default defineConfig({
   plugins: [
@@ -10,10 +9,6 @@ export default defineConfig({
       projects: ["./tsconfig.json"],
     }),
     tailwindcss(),
-    tanstackStart({
-      server: {
-        presets: "vercel",
-      },
-    }),
+    tanstackStart({ target: "vercel" }),
   ],
 });
