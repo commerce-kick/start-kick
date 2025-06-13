cc58849492cf5b2178968acbf273461009239f67
Fermin Villanueva Salas <FerVillanuev4s@gmail.com>
2025-06-12 17:31:25 -0700

    Move to soruce

---
+++ src/integrations/salesforce/server/orders.ts
@@ -0,0 +1,29 @@
+import { getSalesforceAPI } from "@/integrations/salesforce/server/config";
+import { CreateOrderParams } from "@/integrations/salesforce/types/params";
+import { createServerFn } from "@tanstack/react-start";
+
+export const createOrder = createServerFn({ method: "POST" })
+  .validator((data: CreateOrderParams) => data)
+  .handler(async ({ data }) => {
+    const { api, client } = await getSalesforceAPI();
+    const shopperOrders = await api.shopperOrders();
+
+    return await shopperOrders.createOrder({
+      parameters: data.params,
+      //@ts-ignore
+      body: data.body,
+    });
+  });
+
+export const getOrder = createServerFn({ method: "GET" })
+  .validator((data: { orderNo: string }) => data)
+  .handler(async ({ data }) => {
+    const { api } = await getSalesforceAPI();
+    const shopperOrders = await api.shopperOrders();
+
+    return await shopperOrders.getOrder({
+      parameters: {
+        orderNo: data.orderNo,
+      },
+    });
+  });
